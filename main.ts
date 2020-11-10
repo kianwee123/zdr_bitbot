b37cd9d9c3875adac5c59bd9a8a942d185cadaab
kianwee123 <kianwee@163.com>
2020-11-10 17:56:32 +0800

    change names

--- main.ts
+++ main.ts
@@ -161,7 +161,7 @@ namespace bitbot_显示类 {
  ****************************************************************************************************************************************/
 
 //% color="#87CEEB" weight=24 icon="\uf1b6"
-namespace mbit_传感器类 {
+namespace bitbot_传感器类 {
 
     export enum enVoice {
         //% blockId="Voice" block="有声音"
@@ -265,7 +265,7 @@ namespace mbit_传感器类 {
  ****************************************************************************************************************************************/
 
 //% color="#808080" weight=23 icon="\uf11c"
-namespace mbit_输入类 {
+namespace bitbot_输入类 {
 
     export enum enRocker {
         //% blockId="Nostate" block="无"
@@ -378,7 +378,7 @@ namespace mbit_输入类 {
  ****************************************************************************************************************************************/
 
 //% color="#D2691E" weight=22 icon="\uf001"
-namespace mbit_音乐类 {
+namespace bitbot_音乐类 {
     export enum enBuzzer {
 
         //% blockId="NoBeep" block="不响"
@@ -407,7 +407,7 @@ namespace mbit_音乐类 {
  ****************************************************************************************************************************************/
 
 //% color="#0000CD" weight=21 icon="\uf185"
-namespace mbit_电机类 {
+namespace bitbot_电机类 {
 
     //% blockId=mbit_Fan block="Fan|pin %pin|speed %value"
     //% weight=100
@@ -436,7 +436,7 @@ namespace mbit_电机类 {
 }
 
 //% color="#006400" weight=20 icon="\uf1b9"
-namespace mbit_小车类 {
+namespace bitbot_小车类 {
 
     const PCA9685_ADD = 0x41
     const MODE1 = 0x00
